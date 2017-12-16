8e904af897c26cccc6de9474e342c20ec59f8c51
xkkhh <32864981+xkkhh@users.noreply.github.com>
2017-12-16 22:17:21 +0800

    Update spiderbook.py

--- spiderbook.py
+++ spiderbook.py
@@ -1,37 +1,64 @@
 #!/usr/bin/env python
 #coding:utf-8
-#make_by_xkkhh
-#2017年12月16日16:52:10
+#make by xkkhh
+#2017年12月16日21:02:17
 
 import sys
-import requests
 import re
+import requests
 
-def seartxt():
+headers = {'content-type': 'application/json',
+           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+
+def seartxt(xsm, yema = '0'):
     '''
     搜索小说
     爬行笔趣阁的小说,没过多的测试。没问题别找我,有问题更别找我!
     '''
 
-    xsm = raw_input("搜索你要下载的小说名:")
-    r = requests.get('http://zhannei.baidu.com/cse/search?s=17194782488582577862&entry=1&q='+xsm)    #获取搜索小说名
+    r = requests.get('http://zhannei.baidu.com/cse/search?q='+ xsm + '&p='+ yema + '&s=920895234054625192&nsid=', headers)  #获取搜索的小说名
     r.encoding = "utf-8"
-    xsm = re.findall('<a cpos="title" href="(.*) class="result-game-item-title-link" target="_blank">', r.text) #匹配链接
+    #print r.text
+    r1 = re.findall('<a cpos="title" href="(.*)" title="', r.text) #匹配小说链接
+    r2 = re.findall('" title="(.*)" class="', r.text)              #匹配小说名字
+    r3 = re.findall('<span>\s*(.*)\s*</span>', r.text)             #匹配作者
 
-    n = 0
     url = []
+    bookname = []
+    author = []
+    n, n1, n2, n3 = 0, 0, 0, 0
 
-    for x in xsm:
-        x1 = x[0:39]     #小说链接
-        x2 = x[48:-1]    #小说名字
-        print str(n)+ '.' + x2
-        n += 1
-        url.append(x1)
+    for x in r1:
+        url.append(x)
+        n = n + 1
+    for x1 in r2:
+        bookname.append(x1)
+        n1 = n1 + 1
+    for x2 in r3:
+        author.append(x2)
+        n2 = n2 + 1
 
-    userurl = url[input("输入序号下载:")]
-    downloadtxt(userurl)
+    print '第'+yema + '页'
+    while n3 < len(r1):
+        print str(n3) + u'.书名:' + bookname[n3] + u" 作者:" + author[n3]
+        n3 += 1
+    print "c:上一页 n:下一页 i:自定义页 数字:下载小说"
 
-def downloadtxt(weburl):
+    search_ordownload = raw_input(":")
+    if search_ordownload == "c":
+        seartxt(xsm, str(int(yema) - 1))
+    elif search_ordownload == "n":
+        seartxt(xsm, str(int(yema) + 1))
+    elif search_ordownload == "i":
+        search_ordownload = raw_input(":")
+        seartxt(xsm, search_ordownload)
+    elif int(search_ordownload) >= 0:
+        downloadtxt(url[int(search_ordownload)])
+    else:
+        print 'Error'
+        exit()
+
+def downloadtxt(url):
     '''
     下载小说
     爬行笔趣阁的小说,没过多的测试。没问题别找我,有问题更别找我!
@@ -39,42 +66,37 @@ def downloadtxt(weburl):
     reload(sys)
     sys.setdefaultencoding("utf-8")
 
-    r = requests.get(weburl)                 #网站最新章节
-    r.encoding = "gbk"
 
-    txtname = re.findall('<meta property="og:title" content="(.*?)"/>',r.text)[0] #获取小说名
-    dirctory = re.findall('<li><a href="(.*?)">', r.text)                        #获取链接
 
-    for y in dirctory:
-        r1 = requests.get("http://www.bqg5200.com/" +y)
-        r1.encoding = 'gbk'
+    r4 = requests.get(url, headers)
+    dirctory = re.findall('<a style="" href="(.*?)">', r4.text)                         #获取全部章节url
+    chapter = re.findall('">(.*?)</a></dd>', r4.text)                                   #获取全部章节
+    txtname = re.findall('<meta property="og:title" content="(.*?)"/>', r4.text)[0]     #获取书名
+    x = 0
 
-        try:
-            txtchapter = re.findall(u"<title>(.*?)_笔趣阁</title>", r1.text) #获取章节
-        except IndexError,e:
-            print e.message                                                  #异常处理
-            continue
-
-        txtzhang = (txtchapter[0])[(len(txtname))+1:]                       #章节
-        txtstr = re.findall(r'&nbsp;&nbsp;&nbsp;&nbsp;(.+)</div>', r1.text) #章节内容
-
-        f1 = open('./' + txtname + '.txt', "a+")                #检查是否写入过该章节
-        quchong = f1.read()
-        f1.close()
-
-        if (quchong.find(txtzhang) >= 0):
-            print "找到重复章节%s跳过.." %txtchapter[0]
-        elif (quchong.find(txtzhang) == -1):
-            for z in txtstr:
-                print "正在写入%s的%s" % (txtname,txtzhang)
-                z = z.replace("<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;","\n") #获取内容
-                f2 = open('./'+ txtname + '.txt', "a+") #写入文件,追加模式.
-                f2.write(txtzhang + "\n" + z + "\n")
+    while x <= len(dirctory):
+        r5 = requests.get(r'https://www.qu.la' + dirctory[x], headers)
+        booktxt = re.findall(u'&nbsp;&nbsp;&nbsp;&nbsp;(.*?)<br/>', r5.text)            #获取当前章节文字
+        nowchapter = chapter[x]                                                         #当前章节
+        f_check = open('./' + txtname + '.txt', "a+")
+        check = f_check.read()
+        f_check.close()
+        if (check.find(nowchapter) >= 0):
+            print "找到重复章节%s跳过.." %nowchapter
+        elif (check.find(nowchapter) == -1):
+            f1 = open('./' + txtname + '.txt', "a+")
+            f1.write(nowchapter + "\n")                                                #写入当前章节
+            f1.close()
+            print '正在写入%s %s' %(txtname, nowchapter)
+            for nowbooktxt in booktxt:                                                 #当前文字
+                f2 = open('./' + txtname + '.txt', "a+")
+                f2.write(nowbooktxt + "\n")                                            #写入当前文字
                 f2.close()
         else:
             print 'Error!'
-
+        x += 1
     print "写入小说%s完成!" % txtname
 
 if __name__ == "__main__":
-    seartxt()
+    searchname = raw_input("搜索你要下载的小说名:")
+    seartxt(searchname)
