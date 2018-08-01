fe7593607bbe783a3ca7f685ee79602f7430ae44
xkkhh <32864981+xkkhh@users.noreply.github.com>
2018-08-01 22:20:24 +0800

    Update Batch_website_transition_detection.py

--- Batch_website_transition_detection.py
+++ Batch_website_transition_detection.py
@@ -6,18 +6,28 @@ import socket
 import requests
 
 def urlstatucheck():
-   for oneurl in urllist.readlines():
-       #example url:http://blog.xkkhh.cn
-       
-       url=str(oneurl.strip())[7:]
-       try:
-           ip =socket.gethostbyname(url)
-           webstatus = requests.get(url=oneurl.strip(), timeout=10).status_code
-           iplist.writelines(oneurl.strip() + " " + str(ip) + " " + str(webstatus) + "\n")
-       except:
-           print "this url is can not connect"
+    for oneurl in urllist.readlines():
+		cnc = oneurl.replace("\n", "") + " is like can not connect!"
+		if oneurl.startswith("http:"):
+			url=str(oneurl.strip())[7:]
+			try:
+				ip =socket.gethostbyname(url)
+				webstatus = requests.get(url=oneurl.strip(), timeout=10).status_code
+				iplist.writelines(oneurl.strip() + " " + str(ip) + " " + str(webstatus) + "\n")
+			except:
+				print cnc
+		if oneurl.startswith("https:"):
+			url=str(oneurl.strip())[8:]
+			try:
+				ip =socket.gethostbyname(url)
+				webstatus = requests.get(3url=oneurl.strip(), timeout=10).status_code
+				iplist.writelines(oneurl.strip() + " " + str(ip) + " " + str(webstatus) + "\n")
+			except:
+				print cnc
 
 try:
+    #http://www.baidu.com
+    #https://www.baidu.com
     urllist=open("C:/Users/Mr.xkkhh/Desktop/url.txt","r")
     iplist=open("C:/Users/Mr.xkkhh/Desktop/urlstatuscheck.txt","w")
     urlstatucheck()
